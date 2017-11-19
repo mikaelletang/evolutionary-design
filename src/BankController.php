1eac63f3748965dba538de59516ace4c2029577f
Mikaël Létang <mikael.letang@gmail.com>
2017-11-19 17:49:46 +0100

    Controller

---
+++ src/BankController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace BankKata;
+
+class BankController
+{
+
+    /**
+     *
+     */
+    public function __construct()
+    {
+    }
+
+    public function getStatement(int $bankAccountId) : string
+    {
+        return <<<EOF
+Date | Transaction | Balance
+10/10/2017 | 500 | 500
+10/10/2017 | -200 | 300
+10/10/2017 | 300 | 600
+EOF;
+
+    }
+}
---
+++ tests/unit/Tests/BankControllerTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace BankKata;
+
+use PHPUnit\Framework\TestCase;
+
+class BankControllerTest extends TestCase
+{
+    function testGetStatement() {
+        $controller = new BankController();
+        $statement = $controller->getStatement($bankAccountId = 1);
+        $expectedStatement = $expectedStatement = <<< EOF
+Date | Transaction | Balance
+10/10/2017 | 500 | 500
+10/10/2017 | -200 | 300
+10/10/2017 | 300 | 600
+EOF;
+        $this->assertEquals($expectedStatement, $statement);
+    }
+
+}
