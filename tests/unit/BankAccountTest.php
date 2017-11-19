41658813f9c31d76f647970bfeedc0709d2101c6
Mikaël Létang <mikael.letang@gmail.com>
2017-11-19 18:24:54 +0100

    bank account

--- tests/unit/BankAccountTest.php
+++ tests/unit/BankAccountTest.php
@@ -11,6 +11,9 @@ class BankAccountTest extends TestCase
     public function testGetStatement()
     {
         $bankAccount = new BankAccount();
+        $bankAccount->addTransaction('10/10/2017 | 500 | 500');
+        $bankAccount->addTransaction('10/10/2017 | -200 | 300');
+        $bankAccount->addTransaction('10/10/2017 | 300 | 600');
         $expectedStatement = <<< EOF
 Date | Amount | Balance
 10/10/2017 | 500 | 500
@@ -19,5 +22,4 @@ Date | Amount | Balance
 EOF;
         $this->assertEquals($expectedStatement, $bankAccount->getStatement());
     }
-
 }
