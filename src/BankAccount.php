fb2b3694b823dd6496b6b297b029afdc9f59134b
Mikaël Létang <mikael.letang@gmail.com>
2017-11-19 18:50:13 +0100

    fix vocabulary

--- src/BankAccount.php
+++ src/BankAccount.php
@@ -8,7 +8,7 @@ class BankAccount
     public function getStatement()
     {
         return <<< EOF
-Date | Transaction | Balance
+Date | Amount | Balance
 10/10/2017 | 500 | 500
 10/10/2017 | -200 | 300
 10/10/2017 | 300 | 600
--- tests/unit/BankAccountTest.php
+++ tests/unit/BankAccountTest.php
@@ -12,7 +12,7 @@ class BankAccountTest extends TestCase
     {
         $bankAccount = new BankAccount();
         $expectedStatement = <<< EOF
-Date | Transaction | Balance
+Date | Amount | Balance
 10/10/2017 | 500 | 500
 10/10/2017 | -200 | 300
 10/10/2017 | 300 | 600
