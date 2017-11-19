091364f24f02b3954850362f777ebf08742352b7
Mikaël Létang <mikael.letang@gmail.com>
2017-11-19 18:43:34 +0100

    fix vocabulary

--- src/BankController.php
+++ src/BankController.php
@@ -15,7 +15,7 @@ class BankController
     public function getStatement(int $bankAccountId) : string
     {
         return <<<EOF
-Date | Transaction | Balance
+Date | Amount | Balance
 10/10/2017 | 500 | 500
 10/10/2017 | -200 | 300
 10/10/2017 | 300 | 600
