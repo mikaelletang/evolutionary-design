870b4d49b9611d1eb641976e47cc63035654ebfc
Mikaël Létang <mikael.letang@gmail.com>
2017-11-21 16:57:25 +0100

    use consolidated repo

--- tests/unit/BankControllerTest.php
+++ tests/unit/BankControllerTest.php
@@ -6,8 +6,8 @@ use PHPUnit\Framework\TestCase;
 
 class BankControllerTest extends TestCase
 {
-    /** @var string */
-    private $statement;
+    /** @var BankAccount */
+    private $bankAccount;
 
     /** @var BankAccountRepository */
     private $bankAccountRepository;
@@ -15,19 +15,20 @@ class BankControllerTest extends TestCase
     protected function setUp()
     {
         $this->bankAccountRepository = new BankAccountRepository();
-        $this->statement = <<< EOF
-Date | Amount | Balance
-10/10/2017 | 500 | 500
-10/10/2017 | -200 | 300
-10/10/2017 | 300 | 600
-EOF;
-        $this->bankAccountRepository->add($accountId = 1, $this->statement);
+        $this->bankAccount =new BankAccount();
+        $this->bankAccountRepository->add($accountId = 1, $this->bankAccount);
     }
 
     function testGetStatement() {
         $controller = new BankController($this->bankAccountRepository);
         $returnedStatement = $controller->getStatement($accountId = 1);
-        $this->assertEquals($this->statement, $returnedStatement);
+        $expectedStatement = <<< EOF
+Date | Amount | Balance
+10/10/2017 | 500 | 500
+10/10/2017 | -200 | 300
+10/10/2017 | 300 | 600
+EOF;
+        $this->assertEquals($expectedStatement, $returnedStatement);
     }
 
 }
